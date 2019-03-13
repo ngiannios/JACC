8e2361a561a605250ff4798218be54b4d55303b5
Nikos Giannios <ngiannios@gmail.com>
2019-03-13 13:51:35 +0100

    adding port 80

--- restart_clean_db.sh
+++ restart_clean_db.sh
@@ -12,15 +12,15 @@ python3 jacc_client.py -p 83 &
 python3 jacc_client.py -p 84 &
 python3 jacc_client.py -p 85 &
 
-python3 jacc_add_nodes.py -p 81 --nodePort 80
-python3 jacc_add_nodes.py -p 81 --nodePort 82
-python3 jacc_add_nodes.py -p 81 --nodePort 83
-python3 jacc_add_nodes.py -p 81 --nodePort 84
-python3 jacc_add_nodes.py -p 81 --nodePort 85
-python3 jacc_add_nodes.py -p 82 --nodePort 81
-python3 jacc_add_nodes.py -p 83 --nodePort 81
-python3 jacc_add_nodes.py -p 84 --nodePort 81
-python3 jacc_add_nodes.py -p 85 --nodePort 81
 python3 jacc_add_nodes.py -p 80 --nodePort 81
+python3 jacc_add_nodes.py -p 80 --nodePort 82
+python3 jacc_add_nodes.py -p 80 --nodePort 83
+python3 jacc_add_nodes.py -p 80 --nodePort 84
+python3 jacc_add_nodes.py -p 80 --nodePort 85
+python3 jacc_add_nodes.py -p 81 --nodePort 80
+python3 jacc_add_nodes.py -p 82 --nodePort 80
+python3 jacc_add_nodes.py -p 83 --nodePort 80
+python3 jacc_add_nodes.py -p 84 --nodePort 80
+python3 jacc_add_nodes.py -p 85 --nodePort 80
 
 
